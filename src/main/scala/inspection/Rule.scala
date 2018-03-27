d9c2c3eef8a9ba298c5cea658890182ee007a997
Melchior Tharma Thambipillai <melchior.tharma.thambipillai@cern.ch>
2018-03-27 15:00:38 +0200

    refactor rules with hof

--- src/main/scala/inspection/Rule.scala
+++ src/main/scala/inspection/Rule.scala
@@ -27,109 +27,32 @@ case class Rule(
 
 object Rule{
 
-	val nbAttemptsSSH = Rule((schema, rows) => {
-		val nbAttemptsIndex = schema.fieldIndex("auth_attempts")
+	private def makeRule[T](fieldName: String, nullFallBack: T,
+		rowF: (Row, Int) => T, checkF: T => Boolean, commentText: String):Rule = Rule((schema, rows) => {
+		val index = schema.fieldIndex(fieldName)
 		val tags = rows.map(r => {
-			val nb = if(r.isNullAt(nbAttemptsIndex)) 4 else r.getInt(nbAttemptsIndex)
-			val comment = if(nb>=4) "high/unknown nb attempts" else ""
-			(nb>=4, List(comment))
+			val v = if(r.isNullAt(index)) nullFallBack else rowF(r, index)
+			val check = checkF(v)
+			val comment = if(check) commentText else ""
+			(check, List(comment))
 		})
 		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
 			(tag1 || tag2, comment1:::comment2)
 		}
 	})
 
-	val dstPortSSH = Rule((schema, rows) => {
-		val dstPortIndex = schema.fieldIndex("dstport")
-		val tags = rows.map(r => {
-			val port = if(r.isNullAt(dstPortIndex)) -1 else r.getInt(dstPortIndex)
-			val comment = if(port!=22) "dst port not 22" else ""
-			(port!=22, List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
-
-	val versionSSH = Rule((schema, rows) => {
-		val versionIndex = schema.fieldIndex("version")
-		val tags = rows.map(r => {
-			val version = if(r.isNullAt(versionIndex)) -1 else r.getInt(versionIndex)
-			val comment = if(version<2) "version less than 2.x" else ""
-			(version<2, List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
-
-	val unusualDstHostSSH = Rule((schema, rows) => {
-		val dstHostIndex = schema.fieldIndex("dsthost")
-		val tags = rows.map(r => {
-			val dstHost = if(r.isNullAt(dstHostIndex)) "" else r.getString(dstHostIndex)
-			val comment = if(!dstHost.contains("lxplus")) "unusual dst host" else ""
-			(!dstHost.contains("lxplus"), List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
-
-	val maliciousSrcIP = Rule((schema, rows) => {
-		val srcIPIndex = schema.fieldIndex("srcip")
-		val filename = "knownips.txt"
-		val ips = Source.fromFile(filename).getLines.toList
-		val tags = rows.map(r => {
-			val srcIP = if(r.isNullAt(srcIPIndex)) "" else r.getString(srcIPIndex)
-			val comment = if(ips.contains(srcIP)) "known malicious ip" else ""
-			(ips.contains(srcIP), List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
-
-	val unusualClientSSH = Rule((schema, rows) => {
-		val clientIndex = schema.fieldIndex("client")
-		val filename = "clientstats.txt"
-		val clients = Source.fromFile(filename).getLines.toList
-		val tags = rows.map(r => {
-			val client = if(r.isNullAt(clientIndex)) "null" else r.getString(clientIndex)
-			val comment = if(!clients.contains(client)) "unusual client" else ""
-			(!clients.contains(client), List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
-
-	val unusualServerSSH = Rule((schema, rows) => {
-		val serverIndex = schema.fieldIndex("server")
-		val filename = "serverstats.txt"
-		val servers = Source.fromFile(filename).getLines.toList
-		val tags = rows.map(r => {
-			val server = if(r.isNullAt(serverIndex)) "null" else r.getString(serverIndex)
-			val comment = if(!servers.contains(server)) "unusual server" else ""
-			(!servers.contains(server), List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
-
-	val unusualCipherSSH = Rule((schema, rows) => {
-		val cipherIndex = schema.fieldIndex("cipher_alg")
-		val filename = "cipher_algstats.txt"
-		val ciphers = Source.fromFile(filename).getLines.toList
-		val tags = rows.map(r => {
-			val cipher = if(r.isNullAt(cipherIndex)) "null" else r.getString(cipherIndex)
-			val comment = if(!ciphers.contains(cipher)) "unusual cipher" else ""
-			(!ciphers.contains(cipher), List(comment))
-		})
-		tags.tail.foldLeft(tags.head){case ((tag1,comment1),(tag2, comment2)) => 
-			(tag1 || tag2, comment1:::comment2)
-		}
-	})
+	val nbAttemptsSSH = makeRule[Int]("auth_attempts", 4, _.getInt(_), _>=4, "high/unknown nb attempts")
+	val dstPortSSH = makeRule[Int]("dstport", -1, _.getInt(_), _!=22, "dst port not 22")
+	val versionSSH = makeRule[Int]("version", -1, _.getInt(_), _<2, "version less than 2.x")
+	val unusualDstHostSSH = makeRule[String]("dsthost", "", _.getString(_), !_.contains("lxplus"), "unusual dst host")
+	private val maliciousIPs = Source.fromFile("knownips.txt").getLines.toList
+	val maliciousSrcIP = makeRule[String]("srcip", "", _.getString(_), maliciousIPs.contains(_), "known malicious ip")
+	private val clients = Source.fromFile("clientstats.txt").getLines.toList.map(_.split("""\|\|\|""")(0))
+	val unusualClientSSH = makeRule[String]("client", "null", _.getString(_), !clients.contains(_), "unusual client")
+	private val servers = Source.fromFile("serverstats.txt").getLines.toList.map(_.split("""\|\|\|""")(0))
+	val unusualServerSSH = makeRule[String]("server", "null", _.getString(_), !servers.contains(_), "unusual server")
+	private val ciphers = Source.fromFile("cipher_algstats.txt").getLines.toList.map(_.split("""\|\|\|""")(0))
+	val unusualCipherSSH = makeRule[String]("cipher_alg", "null", _.getString(_), !ciphers.contains(_), "unusual cipher")
 
 	val BroSSHRules = List(nbAttemptsSSH, dstPortSSH, versionSSH,
 		unusualDstHostSSH, maliciousSrcIP, unusualClientSSH, unusualServerSSH, unusualCipherSSH)

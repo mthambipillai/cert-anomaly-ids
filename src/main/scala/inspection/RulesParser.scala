9b9350cd66415067b154417ada25bcbbcad0fc85
Melchior Tharma Thambipillai <melchior.tharma.thambipillai@cern.ch>
2018-04-18 15:18:18 +0200

    add no rule found handling

--- src/main/scala/inspection/RulesParser.scala
+++ src/main/scala/inspection/RulesParser.scala
@@ -17,47 +17,46 @@ object RulesParser{
 			parsedJson <- Try(mapper.readValue[Map[String, Object]](json.reader()))
 				.toDisjunction.leftMap(e => "Could not parse json '"+fileName+"'"+e.getMessage)
 			rulesListMaps = parsedJson("rules").asInstanceOf[List[Map[String,Object]]]
-		}yield{
-			rulesListMaps.map{r =>
+			res <- rulesListMaps.traverseU{r =>
 				val name = r("name").asInstanceOf[String]
 				val params = r("params").asInstanceOf[List[String]]
 				val text = r("text").asInstanceOf[String]
 				getRule(name, params, text)
 			}
-		}
+		}yield res
 	}
 
-	private def getRule(name: String, params: List[String], text: String): Rule = name match{
+	private def getRule(name: String, params: List[String], text: String): String\/Rule = name match{
 		case "ssh_auth_attempts" => {
 			val nbAttempts = params.head.toInt
-			SimpleRule.makeSimpleRule[Int]("auth_attempts", nbAttempts, _.getInt(_), _>=nbAttempts, text)
+			SimpleRule.makeSimpleRule[Int]("auth_attempts", nbAttempts, _.getInt(_), _>=nbAttempts, text).right
 		}
 		case "ssh_dstport" => {
-			SimpleRule.makeSimpleRule[Int]("dstport", -1, _.getInt(_), _!=22, text)
+			SimpleRule.makeSimpleRule[Int]("dstport", -1, _.getInt(_), _!=22, text).right
 		}
 		case "ssh_version" => {
 			val version = params.head.toInt
-			SimpleRule.makeSimpleRule[Int]("version", -1, _.getInt(_), _< version, text)
+			SimpleRule.makeSimpleRule[Int]("version", -1, _.getInt(_), _< version, text).right
 		}
 		case "ssh_srcip" => {
 			val knownipsFileName = params.head
-			SimpleRule.makeFileContainsRule(knownipsFileName, "srcip", "", text)
+			SimpleRule.makeFileContainsRule(knownipsFileName, "srcip", "", text).right
 		}
 		case "ssh_dsthost" => {
 			val dsthostsFileName = params.head
-			SimpleRule.makeFileNotContainsRule(dsthostsFileName, "dsthost", "null", text)
+			SimpleRule.makeFileNotContainsRule(dsthostsFileName, "dsthost", "null", text).right
 		}
 		case "ssh_client" => {
 			val clientsFileName = params.head
-			SimpleRule.makeFileNotContainsRule(clientsFileName, "client", "null", text)
+			SimpleRule.makeFileNotContainsRule(clientsFileName, "client", "null", text).right
 		}
 		case "ssh_server" => {
 			val serversFileName = params.head
-			SimpleRule.makeFileNotContainsRule(serversFileName, "server", "null", text)
+			SimpleRule.makeFileNotContainsRule(serversFileName, "server", "null", text).right
 		}
 		case "ssh_cipher" => {
 			val ciphersFileName = params.head
-			SimpleRule.makeFileNotContainsRule(ciphersFileName, "cipher_alg", "null", text)
+			SimpleRule.makeFileNotContainsRule(ciphersFileName, "cipher_alg", "null", text).right
 		}
 		case "ssh_total_auth_attempts" => {
 			val totalNbAttempts = params.head.toInt
@@ -73,7 +72,8 @@ object RulesParser{
 				}else{
 					(false, comments)
 				}
-			})
+			}).right
 		}
+		case _ => ("Unknown rule '"+name+"'").left
 	}
 }
